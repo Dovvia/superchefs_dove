111b69c814b5b5b9bfc017c672e927c00149c1cc
Fadarse <fadarse@gmail.com>
2025-04-25 23:49:38 +0200

    ...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ho47tn3vlag"
+    "revision": "0.8v5ip46cpn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
